5aee59a8a3a4117624baf7002c6c8d3b037b4ff7
Win Ko Ko Hein <winkokoheinkw@gmail.com>
2021-01-19 16:29:43 +0630

    update webhook get method

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ config = require("./service/config"),
 app = express();
 
 
-app.get('/webhook', (req, res) => {
-  
-  const VERIFY_TOKEN = "helloworKd";
-  
+app.get("/webhook", (req, res) => {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === config.verifyToken) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
 });
 
 app.post('/webhook', (req, res) => {  
